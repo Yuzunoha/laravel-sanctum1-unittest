ae802b306feb9b2757cecdd16bb8a39319899ab7
Yuzunoha <studysteadystandby@gmail.com>
2021-10-08 12:59:49 +0900

    php artisan make:factory PostFactory --model=Post

--- html/laravelapp/database/factories/PostFactory.php
+++ html/laravelapp/database/factories/PostFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Model;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -12,7 +12,7 @@ class PostFactory extends Factory
      *
      * @var string
      */
-    protected $model = Model::class;
+    protected $model = Post::class;
 
     /**
      * Define the model's default state.
